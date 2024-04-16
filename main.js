c0ae1b8a9fa17c3c494238f4bc390c735333a485
BlarQ <collinsogunlala@gmail.com>
2024-04-16 15:55:21 +0100

    feat: js first class

---
+++ main.js
@@ -0,0 +1,21 @@
+// alert('Hello Everybody!')
+
+// confirm('Are you coming to the Party?')
+
+// console.log(BlarQ)
+
+// TOPIC: Variables
+
+const num2 = 10
+
+let myName = 'Adedamola'
+
+let fullName = "Adedamola Ogunlala"
+
+let workPlace = `Univelcity`
+
+let isMale = true
+
+let statement = `my name is ${myName} and i was born in ${new Date().getFullYear() - 27} and i work for ${workPlace}. and i am a ${isMale ? 'Male' : 'Female'}`
+
+console.log(statement)
---
+++ string.js
@@ -0,0 +1,15 @@
+//STRING METHODS
+
+const sentence = 'The quick brown fox jumped over the lazy dog'
+
+//LENGTH OF A STRING
+console.log(sentence.length)
+
+//CONVERT TO UPPERCASE
+console.log(sentence.toUpperCase())
+
+//CONVERT TO LOWERCASE
+console.log(sentence.toLowerCase())
+
+//CUT OUT STRING
+console.log(sentence.slice(0, 10))
