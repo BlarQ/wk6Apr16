96cebec5f74df4ee57f99c8b17086167fb5462b4
BlarQ <collinsogunlala@gmail.com>
2024-04-18 13:21:44 +0100

    add: padding

--- about.js
+++ about.js
@@ -2,29 +2,50 @@ const pOne = document.getElementById('intro')
 
 pOne.innerHTML = '<strong>Introduction:</strong> I am Adedamola also called BlarQ. I am a Tech Savvy and student of Univelcity'
 
+pOne.style.fontSize = '30px'
+pOne.style.paddingInline = '20px'
+
 const pTwo = document.getElementsByClassName('body')
 
 pTwo[0].innerHTML = '<strong>Passions and Interests:</strong> I have a deep passion for technology and its ever-evolving landscape, constantly exploring new innovations and developments.'
+
+pTwo[0].style.fontSize = '30px'
+pTwo[0].style.paddingInline = '20px'
 
 const pTwo1 = document.getElementsByClassName('body')
 
 pTwo1[1].innerHTML = '<strong>Professional Background:</strong>  With a background in computer science/engineering, i have honed your skills in software development, perhaps specializing in areas like web development, machine learning, or cybersecurity. I learnt <abbr title="HyperText MarkUp Language">HTML</abbr>, <abbr title="Cascading Style Sheet">CSS</abbr> and still learning <abbr title="JavaScript">JS</abbr>'
 
+pTwo[1].style.fontSize = '30px'
+pTwo[1].style.paddingInline = '20px'
+
 const pTwo2 = document.getElementsByClassName('body')
 
 pTwo2[2].innerHTML = "<strong>Education:</strong> I pursued your education in a reputable institution, earning a degree in a relevant field and possibly continuing your learning journey through online courses or certifications."
+
+pTwo[2].style.fontSize = '30px'
+pTwo[2].style.paddingInline = '20px'
 
 const pTag = document.getElementsByTagName('p')
 
 pTag[4].innerHTML = "<strong>Projects and Contributions:</strong> I've actively contributed to projects within your field, whether through personal projects, open-source contributions, or collaborations with other professionals or organizations."
 
+pTag[4].style.fontSize = '30px'
+pTag[4].style.paddingInline = '20px'
+
 const pTag1 = document.getElementsByTagName('p')
 
 pTag1[5].innerHTML = "<strong>Skills and Expertise:</strong> My skillset extends beyond technical knowledge, encompassing problem-solving abilities, critical thinking, teamwork, and effective communication skills."
 
+pTag1[5].style.fontSize = '30px'
+pTag1[5].style.paddingInline = '20px'
+
 const pTag2 = document.getElementsByTagName('p')
 
 pTag2[6].innerText = 'Hobbies: My hobbies are; \n \n Dancing \n \n Singing \n \n  Coding'
+
+pTag2[6].style.fontSize = '30px'
+pTag2[6].style.paddingInline = '20px'
 
 const head = document.querySelector('h1')
 
@@ -32,5 +53,11 @@ head.style.color = 'blue'
 head.style.textAlign = 'center'
 head.style.borderBottom = '5px solid'
 head.style.backgroundColor = 'yellow'
+head.style.margin = '0'
+
+const body = document.querySelector('body')
+
+body.style.backgroundColor = 'grey'
+body.style.margin = '0'
 
 
